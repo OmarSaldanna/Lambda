004816bb67c0aa9cfce181d7e0eda1edd828554d
OmarSaldanna <ml.omar.py@gmail.com>
2023-03-19 22:43:01 -0600

    max-tokens set to 3096

--- lambda/modules/models/gpt3.py
+++ lambda/modules/models/gpt3.py
@@ -10,8 +10,8 @@ class GPT3:
     response = openai.Completion.create(
       model="text-davinci-003",
       prompt=text,
-      temperature=0,
-      max_tokens=2048,
+      temperature=0.5,
+      max_tokens=3096,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0,
