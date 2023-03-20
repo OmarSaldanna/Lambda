e970aa8b4eb35c01cdab32d3af23877916433835
OmarSaldanna <ml.omar.py@gmail.com>
2023-03-19 22:51:58 -0600

    changed run.sh file

--- run.sh
+++
@@ -1,4 +0,0 @@
-echo "Deploying Lambda λ"
-tmux new-session -d -s lambda 'python3 lambda/app.py'
-tmux new-session -d -s discordo 'python3 discordo/app.py'
-tmux new-session -d -s telegram 'python3 telegram/app.py'
