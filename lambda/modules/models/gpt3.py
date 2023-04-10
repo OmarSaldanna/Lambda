98400e6c25d036cbe990eb8b7f79ebe9c366038c
Omarsaldanna <ml.omar.py@gmail.com>
2023-04-10 01:32:01 +0000

    reboot-lambda and change to curie model

--- lambda/modules/models/gpt3.py
+++ lambda/modules/models/gpt3.py
@@ -8,10 +8,10 @@ class GPT3:
   def __call__(self, text):
     # use gpt-3
     response = openai.Completion.create(
-      model="text-davinci-003",
+      model="text-curie-001",
       prompt=text,
       temperature=0.5,
-      max_tokens=3096,
+      max_tokens=2000,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0,
