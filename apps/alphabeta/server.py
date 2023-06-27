51f2f24f597a05606ba411437f8dd2d4ccb55afa
OmarSaldanna <ml.omar.py@gmail.com>
2023-06-26 18:55:35 -0600

    fixing bugs

--- apps/alphabeta/server.py
+++ apps/alphabeta/server.py
@@ -12,15 +12,18 @@ CORS(app)
 @app.route('/lambda/talk', methods=['GET'])
 def lambda_conversation():
 	if request.method == 'GET':
-		# extract the message from the request
-		message = request.headers.get('message')
-		author = request.headers.get('author')
-		# use the talk
-		if author == "717071120175595631":
-			# get the audio file
-			file_path = modules.talk(message)
-			# 
-			return jsonify({'content': file_path})
+		try:
+			# extract the message from the request
+			message = request.headers.get('message')
+			author = request.headers.get('author')
+			# use the talk
+			if author == "717071120175595631":
+				# get the audio file
+				file_path = modules.talk(message)
+				# 
+				return jsonify({'content': file_path})
+		except:
+			return jsonify({'content': 'error'})
 
 # run the app, on localhost only
 app.run(port=88, host="0.0.0.0", debug=True)
