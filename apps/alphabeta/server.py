3f5875eb44abbfaecf7621a53b258b631fab500b
OmarSaldanna <ml.omar.py@gmail.com>
2023-08-17 10:55:14 -0600

    fixing bugs on alphabeta

--- apps/alphabeta/server.py
+++ apps/alphabeta/server.py
@@ -24,6 +24,7 @@ def alphabeta():
 				# 
 				return jsonify({'content': file_path})
 		except Exception as e:
+			print(e)
 			return jsonify({'content': 'error'})
 
 # run the app, on localhost only
