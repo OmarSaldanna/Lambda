4b50627bc973ab99f21a4b9bdff3382c4ec2045f
OmarSaldanna <ml.omar.py@gmail.com>
2023-09-20 17:51:18 -0600

    new exchange trade mining system

--- daily/scripts/exchange_rates.py
+++ daily/scripts/exchange_rates.py
@@ -1,40 +1,23 @@
-from forex_python.converter import CurrencyRates
 import os
+import json
 
+# data path
 path = "/home/omarsaldanna/Lambda/daily/data/"
-
-def get_rounded_exchange_rates():
-  # Create an instance of CurrencyRates
-  c = CurrencyRates()
-
-  # Define the currencies for which you want to obtain exchange rates
-  currencies = ['EUR', 'IDR', 'BGN', 'GBP', 'DKK', 'CAD', 'JPY', 'HUF', 'RON', 'MYR', 'SEK', 'SGD', 'HKD', 'AUD', 'CHF', 'KRW', 'CNY', 'TRY', 'NZD', 'THB', 'USD', 'NOK', 'INR', 'MXN', 'CZK', 'BRL', 'PLN', 'PHP', 'ZAR']
-
-  # Create a dictionary to store the rounded exchange rates
-  exchange_rates = {}
-
-  # Iterate through the currencies and get their exchange rates with respect to the US Dollar (USD)
-  for currency in currencies:
-    try:
-      # Use the get_rate function to obtain the exchange rate
-      rate = c.get_rate('USD', currency)
-
-      # Round the exchange rate value to 2 decimal places
-      rounded_rate = round(rate, 2)
-
-      exchange_rates[currency] = rounded_rate
-    except Exception as e:
-      exchange_rates[currency] = None
-
-  return exchange_rates
-
-# Call the function to get the rounded exchange rates
-rounded_rates = get_rounded_exchange_rates()
+# admited exchange rates
+exchange_rates = ["MXN","CAD","USD","EUR","JPY","KRW","ARS","BRL","CLP","COP","PEN","UYU","CNY"]
 
 # clear the txt file
 os.system(f'echo "" > {path}exchange_rates.txt')
+# enable the api key
+# and run the request and save it in a json
+os.system(f"source ~/Lambda/.env && curl http://api.exchangeratesapi.io/v1/latest?access_key=$exchangeapi > {path}rates.json")
+# open the .json file
+data = json.load(open(f'{path}rates.json'))
 
-# Print the rounded exchange rates
-for currency, rate in rounded_rates.items():
-  # print them inside an txt
-  os.system(f'echo "{currency}:{rate}" >> {path}exchange_rates.txt')
+# print the exchange rates on a txt
+for rate in data['rates'].keys():
+  # only the selected rates
+  if rate in exchange_rates:  
+    # print them inside an txt
+    value = data['rates'][rate]
+    os.system(f'echo "{rate}:{value}" >> {path}exchange_rates.txt')
---
+++ lambda/skills/converter.py
@@ -0,0 +1,24 @@
+from core.modules import OpenAI
+
+
+# lambda convierte
+def main(params: tuple):
+  # extract the params
+  message, member, server = params
+  # get only the message content
+  # instance openai module
+  openai = OpenAI(member, server)
+  # try to make the answer shorter as possible
+  message += ". Responde únicamente con el resultado."
+  # now call gpt
+  return openai.gpt(message, system="Eres una IA capaz de hacer cálculos", message)
+
+
+# info about the skill
+info = """
+### Converter
+Es un conversor universal de unidades, solo dile a que unidades o código quieres convertir algo y listo. **Este comando no aplica para conversiones a monedas**, para ese caso te recomiendo usar el comando **Intercambia**.
+> **Comando:** Lambda convierte 111010110 a hexadecimal
+> **Ejemplo:** lambda convierte 10 Kg a Libras
+> **Ejemplo:** lambda convierte 124 Km a Millas
+"""
