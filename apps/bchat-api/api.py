8bf8650b8264b37538b2fd02b04543d0765b5400
OmarSaldanna <ml.omar.py@gmail.com>
2023-12-24 01:20:01 -0600

    First version of lambda bchat for IoT applications

---
+++ apps/bchat-api/api.py
@@ -0,0 +1,57 @@
+import json
+import socket
+import asyncio
+import os
+from flask_cors import CORS
+from flask import Flask, request, jsonify
+
+
+# create a socket object
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# connect to the bchat-server
+client_socket.connect(("127.0.0.1", 31416))
+# once connected, send the name
+# the format is ~name
+client_socket.send('+lambda'.encode())
+
+# now the flask app
+app = Flask(__name__)
+CORS(app)
+
+# function to struct the message, returns a string
+# message= id~data
+def struct_message(data: dict):
+    # the message will start with the user id
+    message = '-' + data['from'] + '-' + data['to'] + '-' + json.dumps(data)
+    return message
+
+# function to encrypt the message, returns a string
+def encrypt_message(message: str):
+    return message
+
+# data: {
+#   type: input or output
+#   alias: device alias,
+#   value: info,
+# }
+@app.route('/bchat', methods=['POST'])
+async def bchat():
+    # get the json content
+    data = request.json
+    # post to add info
+    if request.method == 'POST':
+        print('posteado')
+        # then struct the message
+        message = struct_message(data)
+        # encrypt the message
+        message = encrypt_message(message)
+        # send it
+        client_socket.send(message.encode())
+        # and return something
+        return jsonify({'answer': 'ok'})
+
+
+# detect dev mode
+dev = True if os.getenv("dev") == 'yes' else False
+# run the app, on localhost only
+app.run(port=8092, host="127.0.0.1", debug=dev)
---
+++ apps/bchat-server/server.py
@@ -0,0 +1,115 @@
+import socket
+import requests
+import threading
+
+# Define the host and port number to listen on
+host = '0.0.0.0'
+port = 31416
+
+# Create a socket object
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to a specific host and port
+server_socket.bind((host, port))
+
+# Listen for incoming connections
+server_socket.listen()
+
+# Create a list to store the client sockets
+# 'id': socket
+clients = {}
+
+# function to append on logs
+def save_log(message: str, db="bchat"):
+    # send the request on the logs
+    requests.post("http://127.0.0.1:8081/logs", json={
+        "db": db,
+        "data": message
+    })
+
+# save when the bchat server was started
+save_log(f'bchat started on {host}:{port}')
+
+# function to handle cleints
+def handle_client(client_socket, client_address):
+    """Handle a single client connection."""
+    print('New client connected:', client_address)
+    save_log(f"[connected] {client_address}")
+
+    # Add the client socket to the list
+    # clients.append(client_socket)
+
+    while True:
+        # Receive a message from the client
+        message = client_socket.recv(1024).decode()
+
+        # if the message is empty
+        if not message:
+            # save a log to see that it happened
+            save_log(f"[error] empty message from: {client_address}", db="bchat-errors")
+            # and ignore it
+            continue
+
+        # if the message is to regist the user
+        # message: +[id]
+        if message[0] == '+':
+            # select the id
+            name = message[1:]
+            # a little verification for lambda user
+            if name == 'lambda' and client_socket[0] != '127.0.0.1':
+                # save a log to see that it happened
+                save_log(f"[attack] lambda login from: {client_address}", db="bchat-errors")
+                # and ignore it
+                continue
+
+            # regist the user and its socket
+            clients[name] = client_socket
+            # and save on log
+            save_log(f"[registered] {name}: {client_address}")
+
+
+        # SECURITY BREACH: anyone connected on the server can send messages
+        # to other people devices
+
+        # if the message is for broadcast
+        # message: -[from]-[to]-[content]
+        if message[0] == '-':
+            # extract the data
+            _, sender, receiver, content = message.split('-')
+            # security verification: messages from lambda came from 127.0.0.1
+            if sender == 'lambda' and client_socket[0] != '127.0.0.1':
+                # save a log to see that it happened
+                save_log(f"[attack] lambda login from: {client_address}, to: {receiver}, content: {content} ", db="bchat-errors")
+                # and ignore it
+                continue
+
+            # if the message was for lambda
+            if receiver == 'lambda':
+                # then save it in a special log, to then read it
+                save_log(f"{sender}-{receiver}-{content}-", db="bchat-receipts")
+
+            # else, it was from lambda, then deliver the message
+            else:
+                # try to find the socket connection
+                try:
+                    # select the receiver's socket
+                    receiver_socket = clients[receiver]
+                    # and send the content
+                    receiver_socket.send(content.encode())
+                except:
+                    pass
+
+        # 
+        print(message)
+
+    # Remove the client socket from the list
+    clients.remove(client_socket)
+    client_socket.close()
+
+while True:
+    # Accept incoming connections
+    client_socket, client_address = server_socket.accept()
+
+    # Start a new thread to handle the client connection
+    thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+    thread.start()
