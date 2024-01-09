762b637a5d9c09ad36dd8addef14db9aff59e762
OmarSaldanna <ml.omar.py@gmail.com>
2024-01-09 03:14:22 -0600

    lambda skill to add members by mention

---
+++ lambda/skills/admin_append_by_mention.py
@@ -0,0 +1,54 @@
+import requests
+
+# function to extract ids from mentions
+def extract_id(mention: str):
+	return mention[2:-1]
+
+# 0      1   2       3 4   5 6
+# agrega por mencion a pro a @users
+# agrega por arroba a pro a @users
+def main(params: tuple):
+	# catch params
+	message, author, server = params
+	# get the user data
+	user_data = requests.get("http://127.0.0.1:8081/members", json={"db": "members", "id": author}).json()['answer']
+	# verify that the user has an admin role
+	if user_data['role'] != 'admin':
+		# if not, return a warning
+		return [{
+			"type": "error",
+			"content": f"Lo siento <@{author}> no tienes permiso para hacer eso"
+		}]
+	# then the user is an admin
+	# cath the params
+	splited_message = message.split(' ')
+	# the role
+	role = splited_message[4]
+	# users mentions 
+	users_mentions = splited_message[6:]
+	# save each user id extracted from the mentions
+	users = [extract_id(u) for user in users_mentions]
+	# having the list of users and the role
+	# make the post of the users in the role
+	try:
+		# make the request
+		ans = requests.post(
+			"http://127.0.0.1:8081/userlist",
+			json={"role": role, "users": users}
+		).json()['answer']
+		# return the answer if everything okey
+		if ans == 'ok':
+			return [{
+				"type": "error",
+				"content": f"Listo, usuarios agregados, {ans}"
+			}]
+	except:
+		return [{
+				"type": "error",
+				"content": f"Ups, algo salió mal"
+			}]
+
+
+
+# info about the skill
+info = """ya sabes que hace wey, pero te dejo un ejemplo: agrega por [mencion, arroba] a [rol] a @users. RECUERDA QUE SI VAS A AGREGAR A UN ROL NUEVO HAY QUE MODIFICAR: **usages.json** y quizá **userlist.json**"""
