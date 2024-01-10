6a14f4fecff80c6e6c1d9616f60d4474b20c48bf
OmarSaldanna <ml.omar.py@gmail.com>
2024-01-10 13:07:32 -0600

    fixed several bugs

--- lambda/skills/exchange_trade.py
+++ lambda/skills/exchange_trade.py
@@ -12,7 +12,7 @@ def main(params: tuple):
   # instance openai module
   openai = OpenAI(member, server)
   # try to make the answer shorter as possible
-  message += f". Responde UNICAMENTE con el resultado de la CONVERSIÓN, El tipo de cambio actual en EUROS es: {rates}."
+  message += f". Responde UNICAMENTE con el resultado de la CONVERSIÓN y EL NOMBRE DE LA MONEDA, El tipo de cambio actual en EUROS es: {rates}."
   # now call gpt
   return openai.gpt(message, system="Eres una IA capaz de hacer cálculos, si solamente escribo 'pesos' me refiero a pesos mexicanos. HAZ UN CALCULO CORECTO.", context=False, model="gpt-4")
 
@@ -20,7 +20,7 @@ def main(params: tuple):
 # info about the skill
 info = """
 ### Exchange Trader
-Es un conversor de monedas, por el momento solamente acepta las monedas: *Peso Argentino (ARS), Real Brasileño (BRL), Dólar Canadiense (CAD), Peso Chileno (CLP), Yuan Chino (CNY), Peso Colombiano (COP), Euro (EUR), Yen Japonés (JPY), Peso Mexicano (MXN), Sol Peruano (PEN), Dólar Estadounidense (USD) y Peso Uruguayo (UYU)*.
+Es un conversor de monedas, por el momento solamente acepta las monedas: *Dólar Canadiense (CAD), Euro (EUR), Yen Japonés (JPY), Peso Mexicano (MXN), Dólar Estadounidense (USD)*.
 > **Comando:** Lambda intercambia ...
 > **Ejemplo:** lambda intercambia 10 pesos a dolares
 > **Ejemplo:** lambda intercambia 50 euroas a pesos mexicanos
