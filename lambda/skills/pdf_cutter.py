dc6bd691c4af7713f811be630c6a69a170b45351
OmarSaldanna <ml.omar.py@gmail.com>
2024-01-21 19:55:43 -0600

    updated pdf cutter function

--- lambda/skills/pdf_cutter.py
+++ lambda/skills/pdf_cutter.py
@@ -1,4 +1,6 @@
+import json
 import PyPDF2
+import requests
 from core.modules import generate_hash
 
 def extraer_paginas(pdf_path, start_page, end_page, output_path):
@@ -13,32 +15,56 @@ def extraer_paginas(pdf_path, start_page, end_page, output_path):
     with open(output_path, 'wb') as output_pdf:
       writer.write(output_pdf)
 
+def get_user_file(user_id):
+  return user_data = requests.get('http://127.0.0.1:8081/members', json={
+    "db": "members",
+    "id": user_id
+  })['answer']['file']
 
-# 0     1  2   3   4  5  6 7
-# corta el pdf de $id de p1 a p2
-# extrae del pdf de $id de p1 a p2
-# recorta del pdf de $id de p1 a p2
+def set_user_file(user_id, file_hash):
+  requests.put('http://127.0.0.1:8081/members', json={
+    "db": "members",
+    "id": user_id,
+    "data": json.dumps({
+      "file": file_hash
+    })
+  })
+
+# 0     1  2   3  4  5 6 
+# corta el pdf de p1 a p2
+# extrae del pdf de p1 a p2
+# recorta del pdf de p1 a p2
 def main(params: tuple):
   # catch the params
   message, member, server = params
   # split the message
   splited_message = message.split(' ')
   # first get the id
-  file_id = splited_message[4][1:]
+  file_id = get_user_file(member)
   # second the pages
-  page_from, page_to = int(splited_message[6]), int(splited_message[8])
+  page_from, page_to = int(splited_message[4]), int(splited_message[6])
   # get the file path
   file_path = f'lambdrive/documents/{file_id}.pdf'
   # generate a new hash
-  new_hash = generate_hash(f"{file_id}_{splited_message[6]}_{splited_message[8]}")
+  new_hash = generate_hash(f"{file_id}_{splited_message[4]}_{splited_message[6]}")
   # and the detination file_path
   file_destination = f'lambdrive/documents/{new_hash}.pdf'
-  # call the function
-  extraer_paginas(file_path, page_from, page_to, file_destination)
+  # try to call the function
+  try:
+    extraer_paginas(file_path, page_from, page_to, file_destination)
+  # if there was an error then return it
+  except:
+    return [{
+      "type": "error", "content": f"Lo siento <@{member}> no encontramos tu archivo, **subirlo de nuevo podría resolver esto.**"
+    }]
+  # finally the file was saved
+  # then save the new hash in the user db
+  set_user_file(member, new_hash)
+  # and return the file
   return [
     {
       "type": "text",
-      "content": f"Listo, tu PDF de la página {page_from} a la {page_to} está disponible como:```${new_hash}```"
+      "content": f"Listo, tu PDF de la página {page_from} a la {page_to} está disponible para usarse."
     },
     {
       "type": "file",
