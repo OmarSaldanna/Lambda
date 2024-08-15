536057fd22ca6caf73459f385d5cb3ed1722e1ac
OmarSaldanna <ml.omar.py@gmail.com>
2024-08-15 13:34:10 -0600

    first fall progess

--- db/run.sh
+++ db/run.sh
@@ -1,6 +1,17 @@
-#!/bin/bash
+# these files dont have the #!/bin/bash since they are
+# called by lambda by an eval. Since that, these files
+# can also use the variables in lambda file
 
-echo -e "${GREEN}Starting DB...${NC}"
-source .env && source venv/bin/activate && python3 db/app.py
-echo -e "${RED}Stopping DB...${NC}"
-exit 0
+echo -e "${START_COLOR}Starting DB...${NO_COLOR}"
+
+# load the global config and keys
+source .env
+source global.conf
+# load the config
+source db/settings.conf
+# activate the venv
+source venv/bin/activate
+# and run the app
+python3 db/app.py
+
+echo -e "${END_COLOR}Stopping DB...${NO_COLOR}"
